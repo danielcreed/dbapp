68f33fc58eb35b70699320245edd96c55681ec56
danielcreed <danielcreed17@gmail.com>
2022-05-23 23:38:04 -0600

    LAter

--- listings/models.py
+++ listings/models.py
@@ -1,3 +1,27 @@
+from turtle import title
 from django.db import models
+from staff.models import Staff
 
 # Create your models here.
+
+class Listing(models.Model):
+    User = models.ForeignKey(Staff, on_delete=models.DO_NOTHING, blank=True)
+    VendorName = models.CharField(max_length=200)
+    Website = models.CharField(max_length=100)
+   # Country = models.BooleanField(default=False)
+
+    Countries = (
+       ('Canada', 'Canada'),
+       ('USA', 'USA'),
+    )
+
+    Country = models.CharField(max_length=20, choices=Countries, blank=True)
+
+
+    Category = models.CharField(max_length=100)
+    Ranking = models.IntegerField(blank=True)
+    desciption = models.TextField(blank=True)
+    photo_main = models.ImageField(upload_to='photos/%Y/%m/%d/',blank=True)
+    is_published = models.BooleanField(default=True)
+    def __str__(self):
+        return self.VendorName
--- staff/models.py
+++ staff/models.py
@@ -1,3 +1,16 @@
+from distutils.command.upload import upload
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
+
+
+class Staff(models.Model):
+    name = models.CharField(max_length=200)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    desription = models.TextField(blank=True)
+    email = models.EmailField(max_length=50)
+    phone = models.CharField(max_length=50)
+    NewUser_Date = models.DateTimeField(default=datetime.now, blank=True)
+    def __str__(self):
+
+     return self.name
