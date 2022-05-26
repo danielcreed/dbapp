ce657c9ea38a01ff76e3c476d529ca1147edb841
danielcreed <danielcreed17@gmail.com>
2022-05-25 22:14:27 -0600

    NEW

--- listings/models.py
+++ listings/models.py
@@ -7,17 +7,13 @@ from staff.models import Staff
 class Listing(models.Model):
     User = models.ForeignKey(Staff, on_delete=models.DO_NOTHING, blank=True)
     VendorName = models.CharField(max_length=200)
-    Website = models.CharField(max_length=100)
+    Website = models.SlugField(max_length=100 )
    # Country = models.BooleanField(default=False)
-
     Countries = (
        ('Canada', 'Canada'),
        ('USA', 'USA'),
     )
-
     Country = models.CharField(max_length=20, choices=Countries, blank=True)
-
-
     Category = models.CharField(max_length=100)
     Ranking = models.IntegerField(blank=True)
     desciption = models.TextField(blank=True)
